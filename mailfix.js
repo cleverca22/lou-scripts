bbe89b06e38ffe9addab916de036dc011be809a8
michael bishop <cleverca22@gmail.com>
2013-06-25 18:19:30 -0300

    temporary fix for mail performance problems

---
+++ mailfix.js
@@ -0,0 +1,15 @@
+// @version        1
+(function () {
+	var titleWidget = qx.core.Init.getApplication().getTitleWidget();
+	titleWidget.mail = new webfrontend.gui.Mail.ListWidget();
+	for (x in titleWidget.mail) {
+		if (titleWidget.mail.hasOwnProperty(x)) {
+			if (titleWidget.mail[x].classname == 'webfrontend.data.MailHeaderDataModel') {
+				titleWidget.mail[x].setBlockSize(10);
+				console.warn('changed block size');
+				return;
+			}
+		}
+	}
+	console.warn('couldnt find prop');
+})()
