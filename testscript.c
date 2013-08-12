dc5e637e24f43f66c2c75a4b17d75596cd9d65ec
michael bishop <cleverca22@gmail.com>
2013-08-12 01:04:55 -0300

    rewrite it to store the path in a config file, and not change the type

--- testscript.c
+++ testscript.c
@@ -7,29 +7,50 @@
 /*
  * script developer code, to aid in testing things inside the extension
  * to compile: gcc testscript.c -lsqlite3 -o testscript
- * usage example: ./testscript ~/.mozilla/firefox/*.default/lou-log.sqlite merc.user.js
+ * usage example: ./testscript merc.user.js
  * 
  * copies merc.user.js into the sqlite db and forces it to run until the next script update
  */
 
 int main(int argc,char *argv[]) {
-	if (argc != 4) {
-		printf("usage: %s sqlitepath scriptpath type\n",argv[0]);
+	if (argc != 2) {
+		printf("usage: %s scriptpath\n",argv[0]);
+		char buffer2[10];
+		read(0,buffer2,1);
 		return -1;
 	}
+	FILE *config = fopen("config.txt","r");
+	if (!config) {
+		printf("error, you need to put the path to lou-log.sqlite in config.txt\n");
+		char buffer2[10];
+		read(0,buffer2,1);
+		return -3;
+	}
+	char dbpath[1024];
+	int size = fread(dbpath,1,1024,config);
+	if (size < 10) {
+		printf("error, db path '%s' doesnt look valid?\n",dbpath);
+		return -3;
+	}
+	dbpath[size] = 0;
+	while (isspace(dbpath[size-1])) {
+		size--;
+		dbpath[size] = 0;
+	}
+	fclose(config);
 	sqlite3 *db;
-	int err = sqlite3_open_v2(argv[1],&db,SQLITE_OPEN_READWRITE,0);
+	int err = sqlite3_open_v2(dbpath,&db,SQLITE_OPEN_READWRITE,0);
 	if (err != SQLITE_OK) {
-		printf("sqlite error when opening db\n");
+		printf("sqlite error when opening db %d %s\n",size,dbpath);
 		return -2;
 	}
-	FILE *fh = fopen(argv[2],"r");
+	FILE *fh = fopen(argv[1],"r");
 	if (!fh) {
 		printf("error when opening script\n");
 		return -3;
 	}
 	struct stat info;
-	if (stat(argv[2],&info)) {
+	if (stat(argv[1],&info)) {
 		printf("error when statting\n");
 		return -4;
 	}
@@ -45,14 +66,14 @@ int main(int argc,char *argv[]) {
 	}
 	fclose(fh);
 	buffer[done] = 0;
-	char *query = "INSERT OR REPLACE INTO scripts (name,filename,remote_version,installed,localversion,enabled,code,type,prio) VALUES ('local override',?,'9999',1,'9999',1,?,?,10)";
+	char *query = "INSERT OR REPLACE INTO scripts (name,filename,remote_version,installed,localversion,enabled,code,prio) VALUES ('local override',?,'9999',1,'9999',1,?,10)";
 	sqlite3_stmt *statement;
 	err = sqlite3_prepare_v2(db,query,strlen(query),&statement,0);
 	if (err != SQLITE_OK) {
 		printf("sqlite error\n");
 		return -5;
 	}
-	err = sqlite3_bind_text(statement,1,argv[2],-1,0);
+	err = sqlite3_bind_text(statement,1,argv[1],-1,0);
 	if (err != SQLITE_OK) {
 		printf("sqlite error\n");
 		return -6;
@@ -62,11 +83,11 @@ int main(int argc,char *argv[]) {
 		printf("sqlite error\n");
 		return -7;
 	}
-	err = sqlite3_bind_text(statement,3,argv[3],-1,0);
-	if (err != SQLITE_OK) {
-		printf("sqlite error\n");
-		return -11;
-	}
+	//err = sqlite3_bind_text(statement,3,argv[2],-1,0);
+	//if (err != SQLITE_OK) {
+	//	printf("sqlite error\n");
+	//	return -11;
+	//}
 	err = sqlite3_step(statement);
 	if (err != SQLITE_DONE) {
 		printf("sqlite error in step %d\n",err);
@@ -83,5 +104,7 @@ int main(int argc,char *argv[]) {
 		return -10;
 	}
 	printf("script loaded into sqlite and force enabled, f5 ingame to see its effects\n");
+	char buffer2[10];
+	read(0,buffer2,1);
 	return 0;
 }
